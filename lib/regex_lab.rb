63868f2ca0abfdccb3c828fa9e6ee2ff176db011
Yana Ustsinovich <yustinovich@icloud.com>
2018-05-31 05:06:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,7 +9,7 @@ word =~ /^[AEIOUaeiou]/ ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/\bun\Bing/)
+ text.scan(/\w{5}/)
 end
 
 def words_five_letters_long(text)
