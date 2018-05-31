422011ae221311509b42803aaa5ef250865d0d71
Yana Ustsinovich <yustinovich@icloud.com>
2018-05-31 05:02:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,15 @@
 def starts_with_a_vowel?(word)
-
+=begin  if (/^[AEIOUaeiou]/).match(word)
+   return true
+  elsif (/^[^AEIOUaeiou]/).match(word)
+   return false
+  end
+=end
+word =~ /^[AEIOUaeiou]/ ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.scan(/\bun\Bing/)
 end
 
 def words_five_letters_long(text)
@@ -15,5 +21,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+phone =~ /[(]?\d{3}[)-.\s]?\d{3}[)-.\s]?\d{4}/ ? true : false
 end
