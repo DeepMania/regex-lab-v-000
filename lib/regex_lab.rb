b9002f5b90c5a5cd90d22628b44101948d7fb00d
Yana Ustsinovich <yustinovich@icloud.com>
2018-05-31 06:21:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,25 +1,19 @@
 def starts_with_a_vowel?(word)
-=begin  if (/^[AEIOUaeiou]/).match(word)
-   return true
-  elsif (/^[^AEIOUaeiou]/).match(word)
-   return false
-  end
-=end
-word =~ /^[AEIOUaeiou]/ ? true : false
+  word =~ /^[AEIOUaeiou]/ ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/\b(un).+(ing)\b/)
+  text.scan(/\b(un).+(ing)\b/)
 end
 
 def words_five_letters_long(text)
- text.scan(/\b\w{5}\b/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text =~ /^[A-Z].+[.?!]$/ ? true : false
+  text =~ /^[A-Z].+[.?!]$/ ? true : false
 end
 
 def valid_phone_number?(phone)
-phone =~ /[(]?\d{3}[)-.\s]?\d{3}[)-.\s]?\d{4}/ ? true : false
+  phone =~ /[(]?\d{3}[)-.\s]?\d{3}[)-.\s]?\d{4}/ ? true : false
 end
