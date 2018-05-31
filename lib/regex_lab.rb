1f56157d544946eb1d91153366baa74eb396ee63
Yana Ustsinovich <yustinovich@icloud.com>
2018-05-31 06:41:20 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text =~ /\b(un)\w+(ing)\b/ == true
+  if text =~ /\b(un)\w+(ing)\b/ 
     return text
   end
 end
